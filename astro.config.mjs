c6d67db7096b4d386fd22632363dc0cfe60e3e8f
Dhruvan <dhruvan.k05@gmail.com>
2024-06-30 16:50:46 +0400

    update to using a new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,76 @@
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { autolinkConfig } from "./plugins/rehype-autolink-config";
+import rehypeSlug from "rehype-slug";
+import astroI18next from "astro-i18next";
+import alpinejs from "@astrojs/alpinejs";
+import AstroPWA from "@vite-pwa/astro";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nomy.vercel.app",
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "github-dark-dimmed" },
-      gfm: true,
-    }),
-    icon(),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  adapter: vercel({
-    analytics: true,
-  }),
+	site: "https://astros.zank.studio",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		tailwind(),
+		sitemap(),
+		astroI18next(),
+		alpinejs(),
+		AstroPWA({
+			mode: "production",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Astros - Starter Template for Astro with Tailwind CSS",
+				short_name: "Astros",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["*.js"],
+			},
+			devOptions: {
+				enabled: false,
+				navigateFallbackAllowlist: [/^\/404$/],
+				suppressWarnings: true,
+			},
+		}),
+		icon(),
+	],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
+	experimental: {
+		contentCollectionCache: true,
+	},
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,43 +1,25 @@
+// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    coverImage: z.string(),
-    category: z.string(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-  }),
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+	schema: z.object({
+		draft: z.boolean(),
+		title: z.string(),
+		snippet: z.string(),
+		image: z.object({
+			src: z.string(),
+			alt: z.string(),
+		}),
+		publishDate: z.string().transform((str) => new Date(str)),
+		author: z.string().default("YourCompany"),
+		category: z.string(),
+		tags: z.array(z.string()),
+	}),
 });
 
-const docs = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-  }),
-});
-
-const guides = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    published: z.boolean().default(true),
-    featured: z.boolean().default(false),
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-  }),
-});
-
-
-export const collections = { blog , docs, guides };
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+	blog: blogCollection,
+};
